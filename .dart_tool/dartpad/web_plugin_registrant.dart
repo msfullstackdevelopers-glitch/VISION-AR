243b46edcf2dd180992c32457e45243f819f8411
rasfsfsggsg <playerkhan0001@gmail.com>
2026-09-25 16:18:14 +0530

    Deploy Flutter Web

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,38 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:firebase_auth_web/firebase_auth_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_database_web/firebase_database_web.dart';
+import 'package:firebase_storage_web/firebase_storage_web.dart';
+import 'package:flutter_inappwebview_web/web/main.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:mobile_scanner/src/web/mobile_scanner_web.dart';
+import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:smart_auth/smart_auth_web.dart';
+import 'package:url_launcher_web/url_launcher_web.dart';
+import 'package:video_player_web/video_player_web.dart';
+import 'package:webview_flutter_web/webview_flutter_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  FirebaseAuthWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
+  FirebaseDatabaseWeb.registerWith(registrar);
+  FirebaseStorageWeb.registerWith(registrar);
+  InAppWebViewFlutterPlugin.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  MobileScannerWeb.registerWith(registrar);
+  SharedPreferencesPlugin.registerWith(registrar);
+  SmartAuthWeb.registerWith(registrar);
+  UrlLauncherPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
+  WebWebViewPlatform.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
